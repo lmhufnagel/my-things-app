c4f745410f6e8fa3820ac9e5f4f052ad936181bf
lmhufnagel <lmhufnagel@gmail.com>
2018-03-24 10:07:11 -0400

    add import provider

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
