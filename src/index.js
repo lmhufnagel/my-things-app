3695ad9380138e2359c48482da183fb090943a25
lmhufnagel <lmhufnagel@gmail.com>
2018-03-24 10:08:12 -0400

    add import thunk

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
