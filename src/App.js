fb4e4f6e858ec056018a03b4a7937f672a70d535
lmhufnagel <lmhufnagel@gmail.com>
2018-04-08 23:48:12 -0400

    add intro message

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
           <h1 className="App-title">My Things App</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          A list of the things I have
         </p>
       </div>
     );
